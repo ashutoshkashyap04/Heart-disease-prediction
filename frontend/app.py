d942c159dec70b4d41e912b488a5d525d14310ec
ashutoshkashyap04 <kashyapashutoshkashyap@gmail.com>
2026-08-13 16:27:21 +0530

    Changed the API endpoint

--- frontend/app.py
+++ frontend/app.py
@@ -85,7 +85,7 @@ if st.button("Predict"):
     }
     
     response = requests.post(
-        "http://127.0.0.1:8000/predict",
+        "https://heart-disease-prediction-api-ey22.onrender.com/predict",
         json = input_data
     )
     
